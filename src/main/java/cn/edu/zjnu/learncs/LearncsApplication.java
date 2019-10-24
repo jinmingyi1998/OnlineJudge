1903e351a7ea9fd3b05e40ef9a90b13455da7ebb
jinmingyi1998 <jinmingyi1998@sina.cn>
2019-10-24 22:17:12 +0800

    first commit

---
+++ src/main/java/cn/edu/zjnu/learncs/LearncsApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.zjnu.learncs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearncsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearncsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/zjnu/learncs/LearncsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.zjnu.learncs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearncsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
