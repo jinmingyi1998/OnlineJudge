13293777d09ab0eee124abb09f2eba8c8cefae8b
jinmingyi <jinmingyi1998@sina.cn>
2020-02-01 19:13:16 +0800

    add user profile when register

--- src/main/java/cn/edu/zjnu/acm/service/UserService.java
+++ src/main/java/cn/edu/zjnu/acm/service/UserService.java
@@ -1,14 +1,19 @@
 package cn.edu.zjnu.acm.service;
 
 import cn.edu.zjnu.acm.entity.User;
+import cn.edu.zjnu.acm.entity.UserProfile;
+import cn.edu.zjnu.acm.repo.UserProfileRepository;
 import cn.edu.zjnu.acm.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    UserProfileRepository userProfileRepository;
 
     public User getUserById(Long id) {
         return userRepository.findById(id).orElse(null);
@@ -18,10 +23,17 @@ public class UserService {
         return userRepository.findByUsername(username).orElse(null);
     }
 
+    @Transactional
     public User registerUser(User u) {
         if (userRepository.findByUsername(u.getUsername()).isPresent())
             return null;
-        return userRepository.save(u);
+        UserProfile userProfile = new UserProfile();
+        u = userRepository.save(u);
+        if (u==null)
+            return null;
+        userProfile.setUser(u);
+        userProfileRepository.save(userProfile);
+        return u;
     }
 
     public User loginUser(User user) {
