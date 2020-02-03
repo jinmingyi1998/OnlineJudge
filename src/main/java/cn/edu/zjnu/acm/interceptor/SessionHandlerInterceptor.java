91ae258828895cedb057dbb6a8a6f5f912d9d77b
jinmingyi <jinmingyi1998@sina.cn>
2020-02-03 11:54:56 +0800

    admin page request login

--- src/main/java/cn/edu/zjnu/acm/interceptor/SessionHandlerInterceptor.java
+++ src/main/java/cn/edu/zjnu/acm/interceptor/SessionHandlerInterceptor.java
@@ -1,29 +1,35 @@
 package cn.edu.zjnu.acm.interceptor;
 
+import cn.edu.zjnu.acm.entity.Teacher;
 import cn.edu.zjnu.acm.entity.User;
+import cn.edu.zjnu.acm.repo.TeacherRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.validation.constraints.NotNull;
 
 @Slf4j
 public class SessionHandlerInterceptor implements HandlerInterceptor {
+    @Autowired
+    TeacherRepository teacherRepository;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HttpSession session = request.getSession();
         try {
-            @NotNull User user = (User) session.getAttribute("currentUser");
-            log.info("admin page intercepted:" + user.getUsername());
-//            if (user != null&&
-//                    user.getPrivilege()== User.Privilege.admin) {
-            return true;
-//            }
-//            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-//            return false;
+            User user = (User) session.getAttribute("currentUser");
+            if (user != null) {
+                log.info("admin page intercepted:" + user.getId() + user.getUsername());
+                Teacher teacher = teacherRepository.findByUser(user).orElse(null);
+                if (teacher != null)
+                    return true;
+            }
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return false;
         } catch (Exception e) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return false;
