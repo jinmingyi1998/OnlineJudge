c53e92c51f09f5b2309f4e2abd95fbe02fed068e
jinmingyi <jinmingyi1998@sina.cn>
2020-02-04 17:13:51 +0800

    update exception 403

--- src/main/java/cn/edu/zjnu/acm/exception/ForbiddenException.java
+++ src/main/java/cn/edu/zjnu/acm/exception/ForbiddenException.java
@@ -3,6 +3,6 @@ package cn.edu.zjnu.acm.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-
-public class NeedLoginException extends RuntimeException {
+@ResponseStatus(value = HttpStatus.FORBIDDEN)
+public class ForbiddenException extends RuntimeException {
 }
