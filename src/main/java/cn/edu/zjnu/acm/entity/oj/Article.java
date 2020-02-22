aee8ea12e5242c3a539f442a3ccfed05545d6598
jinmingyi <jinmingyi1998@sina.cn>
2020-02-22 14:36:11 +0800

    add Article entity

---
+++ src/main/java/cn/edu/zjnu/acm/entity/oj/Article.java
@@ -0,0 +1,27 @@
+package cn.edu.zjnu.acm.entity.oj;
+
+import cn.edu.zjnu.acm.entity.User;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Data
+@Slf4j
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, columnDefinition = "LONGTEXT DEFAULT ''")
+    private String text;
+    @Column(nullable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private Instant postTime;
+    @OneToOne(optional = false)
+    private User user;
+    @Column(nullable = false, columnDefinition = "INTEGER DEFAULT 0")
+    private Integer up;
+    @Column(nullable = false, columnDefinition = "INTEGER DEFAULT 0")
+    private Integer down;
+}
