bed30321121f6c0a5e5d68c262d820baaa7257d9
jinmingyi <jinmingyi1998@sina.cn>
2020-02-23 14:57:02 +0800

    add teacherOnly option

--- src/main/java/cn/edu/zjnu/acm/config/GlobalStatus.java
+++ src/main/java/cn/edu/zjnu/acm/config/GlobalStatus.java
@@ -2,4 +2,5 @@ package cn.edu.zjnu.acm.config;
 
 public class GlobalStatus {
     public static Boolean maintaining = false;
+    public static Boolean teacherOnly = false;
 }
--- src/main/java/cn/edu/zjnu/acm/repo/TeacherRepository.java
+++ src/main/java/cn/edu/zjnu/acm/repo/TeacherRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
     Optional<Teacher> findByUser(User user);
+
+    boolean existsByUser(User user);
 }
