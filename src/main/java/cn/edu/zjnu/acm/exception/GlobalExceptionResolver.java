27b0121177061e226e68a046eabee454d471b4d7
jinmingyi <jinmingyi1998@sina.cn>
2020-02-26 22:35:52 +0800

    add 400 handler

--- src/main/java/cn/edu/zjnu/acm/exception/GlobalExceptionResolver.java
+++ src/main/java/cn/edu/zjnu/acm/exception/GlobalExceptionResolver.java
@@ -1,13 +1,24 @@
 package cn.edu.zjnu.acm.exception;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolationException;
+
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionResolver {
+    @Data
+    public static class Result {
+        private int code = 200;
+        private String message = "";
+    }
+
     @ExceptionHandler(NeedLoginException.class)
     @ResponseBody
     public String exceptionHandle() {
@@ -18,5 +29,23 @@ public class GlobalExceptionResolver {
     @ResponseBody
     public String unavilableHandle() {
         return "维护中，不可用";
+    }
+
+    @ExceptionHandler({BindException.class, ConstraintViolationException.class})
+    public String validatorExceptionHandler(Exception e) {
+        String msg = e instanceof BindException ? String.valueOf(((BindException) e).getBindingResult())
+                : String.valueOf(((ConstraintViolationException) e).getConstraintViolations());
+        return msg;
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseBody
+    public Result handleBindException(MethodArgumentNotValidException ex) {
+        Result errorResult = new Result();
+        StringBuilder msg = new StringBuilder();
+        ex.getBindingResult().getAllErrors().forEach((e)->{msg.append(e.getDefaultMessage()+"\n");});
+        errorResult.setCode(400);
+        errorResult.setMessage(msg.toString());
+        return errorResult;
     }
 }
