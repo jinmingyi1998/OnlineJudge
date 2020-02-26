98c643f71695ded6ea74d1111d2216ae0cd40f16
jinmingyi <jinmingyi1998@sina.cn>
2020-02-26 19:28:34 +0800

    add judgeController

---
+++ src/main/java/cn/edu/zjnu/acm/controller/JudgeController.java
@@ -0,0 +1,107 @@
+package cn.edu.zjnu.acm.controller;
+
+import cn.edu.zjnu.acm.config.Config;
+import cn.edu.zjnu.acm.entity.oj.Solution;
+import cn.edu.zjnu.acm.exception.NotFoundException;
+import cn.edu.zjnu.acm.service.JudgeService;
+import cn.edu.zjnu.acm.service.SolutionService;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+
+@Slf4j
+@RestController
+public class JudgeController {
+    private final JudgeService judgeService;
+    private final SolutionService solutionService;
+
+    public JudgeController(JudgeService judgeService, Config config, SolutionService solutionService) {
+        this.judgeService = judgeService;
+        this.solutionService = solutionService;
+    }
+
+    @PostMapping("/judge/callback")
+    public String judgeCallback(@RequestBody JudgeController.JudgeCallback callback) {
+        try {
+            log.info(callback.toString());
+            Solution solution = solutionService.getSolutionById(callback.getSubmit_id());
+            if (solution == null) {
+                return "no this id";
+            }
+            if (!solution.getResult().equals(Solution.PENDING)) {
+                return "success";
+            }
+            if (callback.getErr() != null) {
+                if (callback.getErr().equals("CE")) {
+                    solution.setResult(Solution.CE);
+                } else {
+                    solution.setResult(Solution.SE);
+                }
+                solution.setInfo(callback.getInfo());
+                solutionService.updateSolutionResultInfo(solution);
+                return "success";
+            } else if (callback.getResults().size() == 0) {
+                solution.setResult(Solution.SE);
+                solution.setInfo("No results");
+                solutionService.updateSolutionResultInfo(solution);
+                return "success";
+            }
+            int cpu = 0;
+            int memory = 0;
+            int caseNumber = 0;
+            for (int i = 0; i < callback.getResults().size(); i++) {
+                caseNumber = i + 1;
+                JudgeController.JudgeCallback.RunMessage runMessage = callback.getResults().get(i);
+                solution.setResult(runMessage.getRunResult());
+                if (runMessage.getResult() > 3) {
+                    cpu = memory = 0;
+                    break;
+                }
+                cpu = Math.max(cpu, runMessage.getCpu_time());
+                memory = Math.max(memory, runMessage.getMemory());
+            }
+            solution.setCaseNumber(caseNumber);
+            solution.setTime(cpu);
+            solution.setMemory(memory);
+            judgeService.update(solution);
+            return "success";
+        } catch (Exception e) {
+            throw new NotFoundException();
+        }
+    }
+
+    @Data
+    static class JudgeCallback {
+        @NotNull
+        private Long submit_id;
+        private String err;
+        private String info;
+        private ArrayList<RunMessage> results;
+
+        @Data
+        static class RunMessage {
+            public static final String[] code = new String[]{Solution.WA, Solution.AC,
+                    Solution.TLE,
+                    Solution.TLE,
+                    Solution.MLE,
+                    Solution.RE,
+                    Solution.SE};
+            private int cpu_time;
+            private int real_time;
+            private int memory;
+            private int signal;
+            private int exit_code;
+            private int error;
+            private int result;
+
+            public String getRunResult() {
+                return code[result + 1];
+            }
+        }
+    }
+}
--- src/main/java/cn/edu/zjnu/acm/controller/MainController.java
+++ src/main/java/cn/edu/zjnu/acm/controller/MainController.java
@@ -1,17 +1,13 @@
 package cn.edu.zjnu.acm.controller;
 
 import cn.edu.zjnu.acm.config.Config;
-import cn.edu.zjnu.acm.entity.oj.Solution;
-import cn.edu.zjnu.acm.exception.NotFoundException;
 import cn.edu.zjnu.acm.service.JudgeService;
 import cn.edu.zjnu.acm.service.SolutionService;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
 
 @Slf4j
 @CrossOrigin
@@ -36,85 +32,5 @@ public class MainController {
     @GetMapping("/notice")
     public String getNotice() {
         return config.getNotice();
-    }
-
-    @PostMapping("/judge/callback")
-    public String judgeCallback(@RequestBody JudgeCallback callback) {
-        try {
-            log.info(callback.toString());
-            Solution solution = solutionService.getSolutionById(callback.getSubmit_id());
-            if (solution == null) {
-                return "no this id";
-            }
-            if (!solution.getResult().equals(Solution.PENDING)) {
-                return "success";
-            }
-            if (callback.getErr() != null) {
-                if (callback.getErr().equals("CE")) {
-                    solution.setResult(Solution.CE);
-                } else {
-                    solution.setResult(Solution.SE);
-                }
-                solution.setInfo(callback.getInfo());
-                solutionService.updateSolutionResultInfo(solution);
-                return "success";
-            } else if (callback.getResults().size() == 0) {
-                solution.setResult(Solution.SE);
-                solution.setInfo("No results");
-                solutionService.updateSolutionResultInfo(solution);
-                return "success";
-            }
-            int cpu = 0;
-            int memory = 0;
-            int caseNumber = 0;
-            for (int i = 0; i < callback.getResults().size(); i++) {
-                caseNumber = i + 1;
-                JudgeCallback.RunMessage runMessage = callback.getResults().get(i);
-                solution.setResult(runMessage.getRunResult());
-                if (runMessage.getResult() > 3) {
-                    cpu = memory = 0;
-                    break;
-                }
-                cpu = Math.max(cpu, runMessage.getCpu_time());
-                memory = Math.max(memory, runMessage.getMemory());
-            }
-            solution.setCaseNumber(caseNumber);
-            solution.setTime(cpu);
-            solution.setMemory(memory);
-            judgeService.update(solution);
-            return "success";
-        } catch (Exception e) {
-            throw new NotFoundException();
-        }
-    }
-
-    @Data
-    static class JudgeCallback {
-        @NotNull
-        private Long submit_id;
-        private String err;
-        private String info;
-        private ArrayList<RunMessage> results;
-
-        @Data
-        static class RunMessage {
-            public static final String[] code = new String[]{Solution.WA, Solution.AC,
-                    Solution.TLE,
-                    Solution.TLE,
-                    Solution.MLE,
-                    Solution.RE,
-                    Solution.SE};
-            private int cpu_time;
-            private int real_time;
-            private int memory;
-            private int signal;
-            private int exit_code;
-            private int error;
-            private int result;
-
-            public String getRunResult() {
-                return code[result + 1];
-            }
-        }
     }
 }
