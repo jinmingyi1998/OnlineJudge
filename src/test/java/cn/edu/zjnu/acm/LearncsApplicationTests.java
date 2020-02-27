f9e9fec10c0e14537c115466b81778e3c02f0b16
jinmingyi <jinmingyi1998@sina.cn>
2020-02-27 22:02:04 +0800

    update test

--- src/test/java/cn/edu/zjnu/acm/LearncsApplicationTests.java
+++ src/test/java/cn/edu/zjnu/acm/LearncsApplicationTests.java
@@ -1,45 +1,35 @@
 package cn.edu.zjnu.acm;
 
+import cn.edu.zjnu.acm.entity.oj.Solution;
+import cn.edu.zjnu.acm.entity.oj.UserProblem;
+import cn.edu.zjnu.acm.repo.problem.SolutionRepository;
+import cn.edu.zjnu.acm.repo.user.UserProblemRepository;
 import cn.edu.zjnu.acm.repo.user.UserRepository;
-import cn.edu.zjnu.acm.service.SolutionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class LearncsApplicationTests {
     @Autowired
     UserRepository userRepository;
     @Autowired
-    SolutionService solutionService;
+    SolutionRepository solutionRepository;
+    @Autowired
+    UserProblemRepository userProblemRepository;
+
     @Test
     void contextLoads() {
-        String code = generateCode(123);
-        System.out.println(code);
-        System.out.println(decode(code));
-    }
-    private String generateCode(int number){
-        int offset = (int) ((Math.random()*10)%10);
-        StringBuffer sub = new StringBuffer();
-        sub.append((char)(offset+65));
-        for(int i = 1;i<18;i++){
-            char c = (char) (Math.random()*26+65);
-            sub.append(c);
+        List<Solution> solutions = solutionRepository.findAll();
+        for (Solution s : solutions) {
+            if (s.getResult().equals(Solution.AC)) {
+                if (!userProblemRepository.existsAllByUserAndProblem(s.getUser(), s.getProblem())) {
+                    userProblemRepository.save(new UserProblem(s.getUser(), s.getProblem()));
+                }
+            }
         }
-        String str = String.format("%06d",number);
-        for(int i=1;i<=6;i++)
-            sub.setCharAt(offset+i, (char) (str.charAt(i-1)+17+offset));
-        return String.valueOf(sub);
     }
-    private int decode(String s){
-        int offset = s.charAt(0)-65;
-        int result=0;
-        for(int i=1;i<=6;i++)
-        {
-            result*=10;
-            int n = s.charAt(offset+i)-65-offset;
-            result+=n;
-        }
-        return result;
-    }
+
 }
