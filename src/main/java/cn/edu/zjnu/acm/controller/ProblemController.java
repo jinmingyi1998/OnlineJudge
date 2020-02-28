6dc05fafa0bbad55e33567d7b09a08fea24817a3
jinmingyi <jinmingyi1998@sina.cn>
2020-02-28 22:12:03 +0800

    add Analysis

--- src/main/java/cn/edu/zjnu/acm/controller/ProblemController.java
+++ src/main/java/cn/edu/zjnu/acm/controller/ProblemController.java
@@ -1,11 +1,11 @@
 package cn.edu.zjnu.acm.controller;
 
 import cn.edu.zjnu.acm.entity.User;
-import cn.edu.zjnu.acm.entity.oj.Problem;
-import cn.edu.zjnu.acm.entity.oj.Solution;
-import cn.edu.zjnu.acm.entity.oj.Tag;
+import cn.edu.zjnu.acm.entity.oj.*;
+import cn.edu.zjnu.acm.exception.ForbiddenException;
 import cn.edu.zjnu.acm.exception.NeedLoginException;
 import cn.edu.zjnu.acm.exception.NotFoundException;
+import cn.edu.zjnu.acm.repo.problem.AnalysisCommentRepository;
 import cn.edu.zjnu.acm.repo.user.UserProblemRepository;
 import cn.edu.zjnu.acm.service.JudgeService;
 import cn.edu.zjnu.acm.service.ProblemService;
@@ -13,8 +13,10 @@ import cn.edu.zjnu.acm.service.SolutionService;
 import cn.edu.zjnu.acm.service.UserService;
 import cn.edu.zjnu.acm.util.RestfulResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -33,8 +35,13 @@ class ProblemViewController {
     }
 
     @GetMapping("/{id:[0-9]+}")
-    public String showproblem(@PathVariable Long id) {
+    public String showProblem(@PathVariable Long id) {
         return "problem/showproblem";
+    }
+
+    @GetMapping("/article/{id:[0-9]}")
+    public String problemArticle() {
+        return "problem/article";
     }
 }
 
@@ -45,7 +52,6 @@ class ProblemViewController {
 public class ProblemController {
     private static final int PAGE_SIZE = 30;
     private final ProblemService problemService;
-    private final UserProblemRepository userProblemRepository;
     private final UserService userService;
     private final JudgeService judgeService;
     private final SolutionService solutionService;
@@ -53,14 +59,13 @@ public class ProblemController {
 
     public ProblemController(ProblemService problemService, UserProblemRepository userProblemRepository, UserService userService, JudgeService judgeService, SolutionService solutionService, HttpSession session) {
         this.problemService = problemService;
-        this.userProblemRepository = userProblemRepository;
         this.userService = userService;
         this.judgeService = judgeService;
         this.solutionService = solutionService;
         this.session = session;
     }
 
-    public static String checkSubmitFrequncy(HttpSession session, String source) {
+    static String checkSubmitFrequncy(HttpSession session, String source) {
         if (session.getAttribute("last_submit") != null) {
             Instant instant = (Instant) session.getAttribute("last_submit");
             if (Instant.now().minusSeconds(10).compareTo(instant) < 0) {
@@ -73,6 +78,13 @@ public class ProblemController {
         }
         session.setAttribute("last_submit", Instant.now());
         return null;
+    }
+    Problem checkProblemExist(Long pid){
+        Problem problem = problemService.getProblemById(pid);
+        if (problem == null) {
+            throw new NotFoundException("No Problem Found");
+        }
+        return problem;
     }
 
     @GetMapping("")
@@ -159,15 +171,41 @@ public class ProblemController {
     }
 
     @GetMapping("/is/accepted/{pid:[0-9]+}")
-    public RestfulResult checkUserHasAc(@SessionAttribute User currentUser,@PathVariable Long pid){
-        Problem problem=problemService.getProblemById(pid);
-        if (problem==null){
-            throw new NotFoundException();
-        }
+    public RestfulResult checkUserHasAc(@SessionAttribute User currentUser, @PathVariable Long pid) {
+        Problem problem = checkProblemExist(pid);
         return new RestfulResult(200,
                 "success",
-                userProblemRepository.existsAllByUserAndProblem(currentUser,problem));
+                problemService.isUserAcProblem(currentUser,problem));
     }
+
+    @GetMapping("/analysis/{pid:[0-9]+}")
+    public RestfulResult getProblemArticle(@PathVariable Long pid,
+                                           @SessionAttribute User currentUser) {
+        Problem problem = checkProblemExist(pid);
+
+        if (!problemService.isUserAcProblem(currentUser, problem)) {
+            throw new ForbiddenException("View after passing the question");
+        }
+        List<Analysis> analyses = problemService.getAnalysisByProblem(problem);
+        analyses.forEach(a -> a.getUser().hideInfo());
+        return new RestfulResult(200, "success", analyses);
+    }
+    @PostMapping("/analysis/post/{pid:[0-9]+}")
+    public RestfulResult postAnalysis(@PathVariable Long pid,
+                                      @SessionAttribute User currentUser,
+                                      @RequestBody @Validated Analysis analysis){
+        Problem problem = problemService.getProblemById(pid);
+        if (!problemService.isUserAcProblem(currentUser,problem)){
+            throw new ForbiddenException("View after passing the question");
+        }
+        analysis.setUser(currentUser);
+        analysis.setComment(null);
+        analysis.setPostTime(Instant.now());
+        analysis.setProblem(problem);
+        problemService.postAnalysis(analysis);
+        return new RestfulResult(200,"success",null);
+    }
+    //TODO post comment
 
     public static class SubmitCodeObject {
         private String source;
