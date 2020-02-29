d724332dde10394f75e13720ae96c9e099369106
jinmingyi <jinmingyi1998@sina.cn>
2020-02-29 22:17:13 +0800

    fix bug

--- src/main/resources/static/js/comment-component.js
+++ src/main/resources/static/js/comment-component.js
@@ -1,14 +1,14 @@
 comment_group = Vue.component("comment-group", {
     props: ["comments", "level"],
     template: `
-<div class="ui threaded comments">
+<div class="ui small minimal comments">
     <div class="comment" v-for="s in comments" v-if="s.fatherId==null||level!=0">
         <div class="content">
-            <a class="author">{{s.user.name}}</a><div class="metadata"><span class="data">{{s.normalPostTime}}</span></div>
-            <div class="md-text" style="padding: 0"><textarea style="display: none;">{{s.text}}</textarea></div>
+            <a class="author" style="font-size: 1.5em" :href="'/user/'+s.user.id">{{s.user.name}}</a><div class="metadata"><span class="data">{{s.normalPostTime}}</span></div>
+            <div class="md-text" style="padding: 0"><textarea style="display: none;" v-html="s.text"></textarea></div>
             <div class="actions"><a class="reply" v-on:click="s.reply_action(s.id)"><i class="ui at icon"></i>Reply</a></div>
         </div>
-        <comment-group  style="padding-top: 2em" v-if="s.sons.length>0" v-bind:comments="s.sons" v-bind:level="1" v-on:reply="s.reply_action(s.id)"></comment-group>
+        <comment-group v-if="s.sons.length>0" v-bind:comments="s.sons" v-bind:level="1"></comment-group>
     </div>
 </div>`
 });
